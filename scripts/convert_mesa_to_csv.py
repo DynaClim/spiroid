3eaa5a0045e94a20438bd104640f3624381735eb
Mats Esseldeurs <matsesseldeurs@yahoo.com>
2025-09-26 11:52:01 +0200

    Remove STEDA dependency

--- scripts/convert_mesa_to_csv.py
+++ scripts/convert_mesa_to_csv.py
@@ -4,7 +4,6 @@
 #     "mesa-reader",
 #     "numpy",
 #     "pandas",
-#     "steda",
 #     "tqdm",
 # ]
 # ///
@@ -21,15 +20,139 @@ from tqdm import tqdm
 import os
 import sys
 
-from STEDA.MESA import loadMesa
-from STEDA.constants import Rsun, Msun, Lsun
+from astropy.constants import R_sun, M_sun, L_sun
 
+Rsun = R_sun.cgs.value   # Solar radius in CGS units
+Msun = M_sun.cgs.value   # Solar mass in CGS units
+Lsun = L_sun.cgs.value   # Solar luminosity in CGS units
 
 def usage():
     print("""usage: convert_MESA_to_csv.py star_mass mesa_directory
                     star_mass: float mass of star in Msun
                     mesa_directory: path to your mesa directory to load the LOG file from""")
 
+
+# Load Mesa data
+#---------------
+def loadMesa(log: mr.MesaLogDir, profile: int):
+    """ Load the data from the mesa log files and the profile snapshot
+
+    Args:
+        log (MesaLogDir): directory to the mesa log files
+        profile (int): profile number
+
+    Returns:
+        Tuple:
+            Ms (float): Mass of the star
+            Rs (float): Radius of the star
+            Ls (float): Luminosity of the star
+            age (float): Age of the star
+            r (NDArray[single]): radius of the datapoints from the stellar evolution simulation
+            rho (NDArray[single]): density of the datapoints from the stellar evolution simulation
+            mass (NDArray[single]): mass inside the sphere of the datapoints from the stellar evolution simulation
+            N (NDArray[single]): Brunt-Väisälä frequency of the datapoints from the stellar evolution simulation
+            i_int_enve (float): index of the interface of the convective to the radiative zone (convective core radiative envelope)
+            i_int_core (float): index of the interface of the convective to the radiative zone (radiative core convective envelope)
+            lc (NDArray[single]): mixing length of the datapoints from the stellar evolution simulation
+            vc (NDArray[single]): convective velocity of the datapoints from the stellar evolution
+    """
+
+    # Setting the mesa log director
+    h = log.history_data
+    p = log.profile_data(profile_number=profile)
+
+    # Retreiving the mass and radius of the star at the given profile snapshot
+    Ms  = h.data_at_model_number("star_mass",log.model_with_profile_number(profile))*Msun   # cgs
+    Rs  = h.data_at_model_number("radius",log.model_with_profile_number(profile))*Rsun      # cgs
+    Ls  = h.data_at_model_number("luminosity",log.model_with_profile_number(profile))*Lsun  # cgs
+    age = h.data_at_model_number("star_age",log.model_with_profile_number(profile))         # yr
+
+    # Retreiving the internal structure profiles for the given profile snapshot
+    r         = np.flip(p.rmid)*Rsun                              # cgs
+    rho       = np.flip(p.rho)                                    # cgs
+    mass      = np.flip(p.mass)*Msun                              # cgs
+    N2        = np.flip(p.brunt_N2)                               # cgs
+    N         = np.sqrt(np.where(N2 < 0, 0, N2))                  # cgs
+    Kt        = np.flip(p.thermal_diffusivity)                    # cgs
+    Kt        = np.where(N2 < 0, 0, Kt)                           # cgs
+    lc        = np.flip(p.mlt_mixing_length)                      # cgs
+    vc        = np.flip(p.conv_vel)                               # cgs
+
+    i_int_core, i_int_enve = calcTriLayer(N2, r, Rs)
+
+    Kt = np.where(r < r[i_int_core], Kt, 0.) # Only take into account the radiative zone
+
+    return Ms, Rs, Ls, age, r, rho, mass, N, i_int_enve, i_int_core, lc, vc
+
+def calcTriLayer(N2, r, Rs):
+    """ Calculate the indices of the interfaces between convective and radiative zones
+
+    Args:
+        N2 (NDArray[single]): squared Brunt-Väisälä frequency
+        r (NDArray[single]): radius of the datapoints
+        Rs (float): radius of the star
+
+    Returns:
+        Tuple[int, int]: indices of the interfaces between convective and radiative zones
+    """
+
+    # Calculate the interaction layers
+    i_int_core, i_int_enve = 0, 0
+    a = np.array(N2)
+    asign = np.sign(a)
+    asignroll = np.roll(asign, 1)
+    asignroll[0] = 1 # to catch if the core is convective or radiative
+    signchange = ((asignroll - asign) != 0).astype(int)
+    where = np.where(signchange==1)[0]
+    if len(where) == 0: # fully convective
+        i_int_enve = 0
+        i_int_core = 0
+    elif where[0] == 0: # radiative core
+        if len(where) == 1: # fully radiative
+            i_int_enve = len(r)-1
+            i_int_core = len(r)-1
+        else: # convective envelope
+            i_int_enve = 0
+            i_int_core = 0
+            for j in range(1, len(where)-1, 2): # convective core should be larger than 1e-4 of the star
+                if r[where[j]] > 1e-4*Rs and (r[where[j+1]] - r[where[j]]) / r[where[j]] > 0.3:
+                    i_int_enve = where[j]
+                    break
+            for i in range(j+1, len(where), 2): # take the first one that is not due to numerical instabilities
+                if (r[where[i]] - r[where[i-1]]) / r[where[i]] > 0.4:
+                    i_int_core = where[i]
+                    break
+    else: # convective core
+        i_int_enve = 0
+        i_int_core = 0
+        where = list(where)
+        where.append(len(r)-1)
+        i = 0
+        for i in range(len(where)-1, 1, -1): # take the first one that is not due to numerical instabilities
+            if (r[where[i]] - r[where[i-1]]) / r[where[i-1]] > 0.3:
+                if (i%2 == 1): i = i-1
+                for j in range(i, 0, -2): # Take the first one inside the numerical instability
+                    if (r[where[j]] - r[where[j-1]]) / r[where[j-1]] > 0.3:
+                        i = j
+                        break
+                    else:
+                        i = j-2
+                i_int_core = where[i]
+                break
+        if i_int_core == 0: i_int_core = where[0]
+        for i in range(i, 0, -2): # take the first one that is not due to numerical instabilities
+            if (r[where[i]] - r[where[i-1]]) / r[where[i-1]] > 0.3:
+                for j in range(i, 0, -2): # check if there is a numerical instability in the shell
+                    if j > 1:
+                        if (r[where[j-1]] - r[where[j-2]]) / r[where[j-1]] > 0.3:
+                            if j == 2: i_int_enve = where[i-1] # If j = 2, the numerical error was inside the convective shell
+                            else:      i_int_enve = where[j-1] # If j > 2, the numerical error was outside the convective shell
+                            break
+                    else:
+                        i_int_enve = where[i-1]
+                        break
+
+    return i_int_core, i_int_enve
 
 def convert_values(mass, log):
     properties = {
@@ -51,18 +174,10 @@ def convert_values(mass, log):
             Rs,
             Ls,
             age,
-            fase,
             r,
             rho,
-            drho,
-            rho_mean,
             mass,
-            g,
-            dg,
-            dN2,
             N,
-            dN,
-            Kt,
             i_int_enve,
             i_int_core,
             lc,
